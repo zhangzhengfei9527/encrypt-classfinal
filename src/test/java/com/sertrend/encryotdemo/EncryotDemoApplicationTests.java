cd10d9a6477a61f49e017adb858dffb8afbca888
zhangzhengfei9527 <zhangzhengfei9527@163.com>
2024-10-19 16:22:14 +0800

    classfinal反编译代码初始化

---
+++ src/test/java/com/sertrend/encryotdemo/EncryotDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sertrend.encryotdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncryotDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
